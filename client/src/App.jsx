ce91f3e3b77cbc4d6de664f863b3be79197951f2
Rensog878 <stephane17081999@gmail.com>
2026-09-05 02:48:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,8 +11,7 @@ import "./App.css";
 // =====================================================
 
 const API_URL =
-  import.meta.env.VITE_API_URL ||
-  "http://localhost:5000";
+  import.meta.env.VITE_API_URL;
 
 console.log("API URL:", API_URL);
 
